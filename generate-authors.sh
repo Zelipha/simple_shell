7062533259be392b15875bf1e1a5aabe44efe4bc
Zelipha <zeliphawambui756@gmail.com>
2021-10-27 13:10:11 -0700

    Modified file

--- generate-authors.sh
+++ generate-authors.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-cd "$(dirname "$(readlink -f "$BASH_SOURCE")")/.."
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")/."
 
 # see also ".mailmap" for how email addresses and names are deduplicated
 
 {
 	cat <<- 'EOH'
-		# This file lists all individuals having contributed content
-		  to the repository.
+		# This file lists all individuals having contributed content to the repository.
 		# For how it is generated, see `generate-authors.sh`.
 	EOH
 	echo
